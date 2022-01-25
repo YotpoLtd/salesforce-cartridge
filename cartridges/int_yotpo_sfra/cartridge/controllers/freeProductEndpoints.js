57403f4e58b8985dcabe4c034ce3b5d617afeab6
Mike Eldred <mikee@cqlcorp.com>
2022-01-25 11:26:55 -0500

    Begin refactoring. Work in progress.

--- cartridges/int_yotpo_sfra/cartridge/controllers/freeProductEndpoints.js
+++ cartridges/int_yotpo_sfra/cartridge/controllers/freeProductEndpoints.js
@@ -1,17 +1,42 @@
+var HTTP_ERROR_STATUS = 422;
+
+/**
+ * AddProductToCart : Helps the creation of new cart item via the Yotpo JavaScript toolset. 
+ * Currently, OCAPI cannot create an item if the user's cart doesn't exist yet. 
+ * This is a workaround. Moments later, an OCAPI call from Yotpo updates the price of the newly created item.
+ * 
+ * Expects a querystring parameter "sku" cooresponding to the productID of the "free" product being added.
+ * 
+ * @name AddProductToCart
+ * @function
+ * @param {middleware} - server.middleware.include
+ * @param {middleware} - cache.applyDefaultCache
+ * @param {category} - non-sensitive
+ * @param {renders} - json
+ * @param {serverfunction} - get
+ */
 server.get('AddProductToCart', server.middleware.https, function (req, res, next) {
     var BasketMgr = require('dw/order/BasketMgr');
     var YotpoLogger = require('*/cartridge/scripts/utils/yotpoLogger');
-    YotpoLogger.logMessage('Received request to fetch single customer',
-        'debug', 'YotpoLoyalty~GetCustomer');
-    var calculator = require('app_storefront_base/cartridge/scripts/hooks/cart/calculate');
-    var Transaction = require('dw/system/Transaction');
-    var ProductMgr = require('dw/catalog/ProductMgr');
-    var basket = BasketMgr.getCurrentOrNewBasket();
-    var sku = req.querystring.sku
+    YotpoLogger.logMessage('Received request to add a free product to the cart',
+        'debug', 'YotpoLoyalty~FreeProduct~AddProductToCart');    
+    var sku = req.querystring.sku;    
+
+    if (empty(sku)) {
+        // bail out if no sku presnt
+        res.setStatusCode(HTTP_ERROR_STATUS);
+        res.json({error_message: 'expecting "sku" parameter in querystring'});
+        next();
+        return;
+    }
 
     if (ProductMgr.getProduct(sku)) {
+        var Transaction = require('dw/system/Transaction');
+        var ProductMgr = require('dw/catalog/ProductMgr');
+        var calculator = require('app_storefront_base/cartridge/scripts/hooks/cart/calculate');
         Transaction.wrap(function() {
-            try {
+            try {                                
+                var basket = BasketMgr.getCurrentOrNewBasket();
                 var lineitem = basket.createProductLineItem(sku, basket.shipments[0]);
                 lineitem.setQuantityValue(1);
                 calculator.calculate(basket);
@@ -20,12 +45,12 @@ server.get('AddProductToCart', server.middleware.https, function (req, res, next
                 res.json({basket_id: basket.UUID});
                
             } catch (e) {
-                res.setStatusCode(422);
+                res.setStatusCode(HTTP_ERROR_STATUS);
                 res.json({error_message: e.message});
             }
         });
     } else {
-        res.setStatusCode(422);
+        res.setStatusCode(HTTP_ERROR_STATUS);
         res.json({error_message: 'product does not exist'});
     }
 
