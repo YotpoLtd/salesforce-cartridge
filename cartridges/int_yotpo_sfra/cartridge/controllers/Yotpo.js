85ca9dda985182139d2a0c0b297fc47cdb053b05
Mike Eldred <mikee@cqlcorp.com>
2022-02-07 15:43:01 -0500

    Refactor. Move customer email token logic to CommonModel.
    This logic now more closely matches that which is in the legacy cartridge.

--- cartridges/int_yotpo_sfra/cartridge/controllers/Yotpo.js
+++ cartridges/int_yotpo_sfra/cartridge/controllers/Yotpo.js
@@ -30,25 +30,7 @@ server.get('IncludeLoyaltyTracking', function (req, res, next) {
     };
 
     if (req.currentCustomer.profile) {
-        customerDetails = CommonModel.getLoggedInCustomerDetails(req.currentCustomer.profile.customerNo);
-
-        // Augment customerDetails with ciphered Customer Email Token.
-        // Token equates to: SHA256(Email + YotpoLoyaltyAPIKey)
-        var YotpoLogger = require('*/cartridge/scripts/utils/yotpoLogger');
-        var MessageDigest = require('dw/crypto/MessageDigest');
-        var Bytes = require('dw/util/Bytes');
-        var Encoding = require('dw/crypto/Encoding');
-        var yotpoConfiguration = YotpoConfigurationModel.getYotpoConfig(currentLocaleID);
-        var cipheredCustomerEmailToken = '';
-        try {
-            var loyaltyAPIKey = yotpoConfiguration.yotpoLoyaltyAPIKey;
-            var messageDigest = new MessageDigest(MessageDigest.DIGEST_SHA_256);
-            var clearTextCustomerEmailToken = customerDetails.customerEmail + loyaltyAPIKey;
-            cipheredCustomerEmailToken = Encoding.toBase64(messageDigest.digestBytes(new Bytes(clearTextCustomerEmailToken, 'UTF-8')));
-        } catch (ex) {
-            YotpoLogger.logMessage('Exception occurred while generating the cipheredCustomerToken Locale: ' + currentLocaleID + ' exception is:' + ex, 'error', 'Yotpo-IncludeLoyaltyTracking');
-        }
-        customerDetails.token = cipheredCustomerEmailToken;
+        customerDetails = CommonModel.getLoggedInCustomerDetails(req.currentCustomer.profile.customerNo, currentLocaleID);
     }
     basketDetails = CommonModel.getCurrentBasketDetails(currentLocaleID);
 
